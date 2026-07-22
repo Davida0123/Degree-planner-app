2bcdcdf5fe9136a5f66038f77f2766f1e1ca93ea
David A <ademoyodavid@gmail.com>
2026-07-22 15:31:33 -0600

    feature: add skeleton scraper with course fields as values in dictionary

--- source/scraper.py
+++ source/scraper.py
@@ -2,4 +2,22 @@ from bs4 import BeautifulSoup
 import requests 
 
 url = "https://calendar.macewan.ca/course-descriptions/cmpt/"
-response = requests.get(url)
+page = requests.get(url)
+soup = BeautifulSoup(page.text, 'html.parser')
+course_dict = {}
+courses = soup.find_all('div', class_ = "courseblock")
+for course in courses:
+    str = course.find('strong').text
+    list = str.split('\n')
+    ccode = list[0].replace('\xa0', ' ')
+    title = list[1]
+    credits = list[2][:1]
+    schedule = '(' + list[2][-6:] + ')'
+    desc = course.find(class_ = "courseblockdesc noindent").text
+    check_preq = course.find('p', class_ = "courseblockextra noindent")
+    if check_preq:
+        preq = check_preq.text.replace('Prerequisites: ', '').replace('\xa0', ' ')
+    else:
+        preq = 'NONE'
+    course_dict.update({ccode:{'Title':title, 'Credits':credits, 'Schedule':schedule, 'Description':desc, 'Prerequisites': preq}})
+    print(course_dict)
